49a7b34027b3b79769bf3a92de9e100e8cd78e42
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2022-12-21 01:15:35 -0300

    Criando classe de previdência
    Co-Authored-By: Lameque Fernandes <79016306+LamequeFernandes@users.noreply.github.com>
    Co-Authored-By: Vitor Diniz Pagani Vieira Ribeiro <vitordiniz25@gmail.com>
    Co-Authored-By: Rafael Leão <54643266+Rafaelltm@users.noreply.github.com>

---
+++ src/model/cadastro/PrevidenciaOficial.java
@@ -0,0 +1,13 @@
+package model.cadastro;
+
+public class PrevidenciaOficial {
+
+    private String descricao;
+    private float valor;
+
+    public PrevidenciaOficial(String descricao, float valor) {
+        super();
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+}
