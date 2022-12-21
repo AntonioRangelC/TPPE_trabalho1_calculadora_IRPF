4a40b4a716fa510e6bf488b13d35f3504ce250d1
Rafaelltm <rafaelltm10@hotmail.com>
2022-12-21 15:47:35 -0300

    (GREEN) calculo de imposto das faixas de 1 a 5

--- src/testes/CalculoImpostosTestes/CalculoImpostosTeste.java
+++ src/testes/CalculoImpostosTestes/CalculoImpostosTeste.java
@@ -11,9 +11,14 @@ import static org.junit.Assert.assertEquals;
 public class CalculoImpostosTeste {
 
     Calculo calculo;
+    Deducao deducao;
+    Rendimento rendimento;
     @Before
     public void setup() {
         calculo = new Calculo();
+
+        Deducao.deducoes.clear();
+        Rendimento.rendimentos.clear();
     }
 
     @Test
@@ -27,5 +32,57 @@ public class CalculoImpostosTeste {
 
         calculo.calcularDeducoes(Deducao.getDeducoes(),Rendimento.getRendimentos());
         assertEquals(0f, calculo.calculaImposto(), 0f);
+    }
+
+    @Test
+    public void calculoTotalImpostosFaixa2() {
+        Deducao deducao = new Deducao("Previdência", 0f);
+        Deducao.deducoes.add(deducao);
+
+        Rendimento rendimento = new Rendimento("Salário", 2437.40f);
+        Rendimento.rendimentos.add(rendimento);
+
+
+        calculo.calcularDeducoes(Deducao.getDeducoes(),Rendimento.getRendimentos());
+        assertEquals(40f, calculo.calculaImposto(), 0.0099f);
+    }
+
+    @Test
+    public void calculoTotalImpostosFaixa3() {
+        Deducao deducao = new Deducao("Previdência", 102f);
+        Deducao.deducoes.add(deducao);
+
+        Rendimento rendimento = new Rendimento("Salário", 3000f);
+        Rendimento.rendimentos.add(rendimento);
+
+
+        calculo.calcularDeducoes(Deducao.getDeducoes(),Rendimento.getRendimentos());
+        assertEquals(79.9f, calculo.calculaImposto(), 0.0099f);
+    }
+
+    @Test
+    public void calculoTotalImpostosFaixa4() {
+        Deducao deducao = new Deducao("Previdência", 101.5f);
+        Deducao.deducoes.add(deducao);
+
+        Rendimento rendimento = new Rendimento("Salário", 4000f);
+        Rendimento.rendimentos.add(rendimento);
+
+
+        calculo.calcularDeducoes(Deducao.getDeducoes(),Rendimento.getRendimentos());
+        assertEquals(241.03f, calculo.calculaImposto(), 0.0099f);
+    }
+
+    @Test
+    public void calculoTotalImpostosFaixa5() {
+        Deducao deducao = new Deducao("Previdência", 1200f);
+        Deducao.deducoes.add(deducao);
+
+        Rendimento rendimento = new Rendimento("Salário", 10000f);
+        Rendimento.rendimentos.add(rendimento);
+
+
+        calculo.calcularDeducoes(Deducao.getDeducoes(),Rendimento.getRendimentos());
+        assertEquals(1550.64f, calculo.calculaImposto(), 0.0099f);
     }
 }
