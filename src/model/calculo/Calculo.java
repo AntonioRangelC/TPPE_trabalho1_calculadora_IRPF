f73919eae29c38e32c96f1af14fb24a81a95429f
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2022-12-21 21:37:08 -0300

    Fase de falsificação, testes falhando

--- src/model/calculo/Calculo.java
+++ src/model/calculo/Calculo.java
@@ -7,6 +7,23 @@ import java.util.ArrayList;
 
 public class Calculo {
     float totalDeducoes;
+
+    public float getTotalImpostos() {
+        return totalImpostos;
+    }
+
+    float totalImpostos;
+
+    public void setAliquotaEfetiva(float aliquotaEfetiva) {
+        this.aliquotaEfetiva = aliquotaEfetiva;
+    }
+
+    float aliquotaEfetiva;
+
+    public float getAliquotaEfetiva() {
+        return aliquotaEfetiva;
+    }
+
     //Base de cálculo
     public float calcularDeducoes(ArrayList<Deducao> deducoes, ArrayList<Rendimento> rendimentos){
 
@@ -74,7 +91,9 @@ public class Calculo {
             valorDaFaixa = Math.min(baseDeCalculo - VALOR_LIMITE_FAIXA1, VALOR_LIMITE_FAIXA2);
             totalImpostos += valorDaFaixa * porcentagemFaixa2;
         }
-
+        this.totalImpostos = totalImpostos;
         return totalImpostos;
     }
+
+
 }
